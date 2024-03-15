318f50fa74296b037f2b92a3b9bf73026ccd2a2b
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-03-15 21:24:53 +0200

    feat: :sparkles: Added guard clauses for API responses
    Checked if the response from the APi is defined before processing the response.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -27,32 +27,38 @@ function TopManga() {
     console.error("An unexpected error occurred: " + error);
   }
 
-  if (topMangaResults) {
-    const topTenManga: RankDetails[] = topMangaResults.slice(0, 10);
-
+  if (!topMangaResults) {
     return (
-      <div className="top-manga-container w-full h-3/10 carousel carousel-center p space-x-4 bg-neutral rounded-box bg-transparent overflow-y-hidden flex align-center py-4 mb-4">
-        {topTenManga.map((mangaRec) => (
-          <div
-            key={mangaRec.slug}
-            className="carousel-item w-2/6 h-full p-0 m-0 flex flex-col justify-center relative indicator first:pl-0 hover:cursor-pointer"
-          >
-            <span className="indicator-item badge badge-secondary px-2.5 py-3.5 border-none bg-purple-1000 text-white font-semibold font-sans">
-              {topMangaResults.indexOf(mangaRec) + 1}
-            </span>
-            <img
-              src={`https://meo3.comick.pictures/${mangaRec.md_covers[0].b2key}`}
-              alt="Manga Cover Image"
-              className="rounded-box h-full w-full shadow-custom"
-            ></img>
-            <div className="image-ovelay h-full w-full rounded-box bg-gradient-to-t from-indigo-950 absolute flex items-end justify-center text-center p-0 m-0  s">
-              <p className="p-2 font-sans text-white">{mangaRec.title}</p>
-            </div>
-          </div>
-        ))}
+      <div className="top-manga-container w-full h-3/10 text-center">
+        <p className="p-2 font-sans text-white">There is no manga to display</p>
       </div>
     );
   }
+
+  const topTenManga: RankDetails[] = topMangaResults.slice(0, 10);
+
+  return (
+    <div className="top-manga-container w-full h-3/10 carousel carousel-center p space-x-4 bg-neutral rounded-box bg-transparent overflow-y-hidden flex align-center py-4 mb-4">
+      {topTenManga.map((mangaRec) => (
+        <div
+          key={mangaRec.slug}
+          className="carousel-item w-2/6 h-full p-0 m-0 flex flex-col justify-center relative indicator first:pl-0 hover:cursor-pointer"
+        >
+          <span className="indicator-item badge badge-secondary px-2.5 py-3.5 border-none bg-purple-1000 text-white font-semibold font-sans">
+            {topMangaResults.indexOf(mangaRec) + 1}
+          </span>
+          <img
+            src={`https://meo3.comick.pictures/${mangaRec.md_covers[0].b2key}`}
+            alt="Manga Cover Image"
+            className="rounded-box h-full w-full shadow-custom"
+          ></img>
+          <div className="image-ovelay h-full w-full rounded-box bg-gradient-to-t from-indigo-950 absolute flex items-end justify-center text-center p-0 m-0  s">
+            <p className="p-2 font-sans text-white">{mangaRec.title}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 function TopManhwa() {
@@ -65,32 +71,40 @@ function TopManhwa() {
     console.error("An unexpected error occurred: " + error);
   }
 
-  if (topManhwaResults) {
-    const topTenManhwa: RankDetails[] = topManhwaResults.slice(0, 10);
-
+  if (!topManhwaResults) {
     return (
-      <div className="top-manhwa-container w-full h-3/10 carousel carousel-center p space-x-4 bg-neutral rounded-box bg-transparent overflow-y-hidden flex align-center py-4 mb-4">
-        {topTenManhwa.map((manhwaRec) => (
-          <div
-            key={manhwaRec.slug}
-            className="carousel-item w-2/6 h-full p-0 m-0 flex flex-col justify-center relative indicator first:pl-0 hover:cursor-pointer"
-          >
-            <span className="indicator-item badge badge-secondary px-2.5 py-3.5 border-none bg-purple-1000 text-white font-semibold font-sans">
-              {topManhwaResults.indexOf(manhwaRec) + 1}
-            </span>
-            <img
-              src={`https://meo3.comick.pictures/${manhwaRec.md_covers[0].b2key}`}
-              alt="Manga Cover Image"
-              className="rounded-box h-full w-full shadow-custom"
-            ></img>
-            <div className="image-ovelay h-full w-full rounded-box bg-gradient-to-t from-indigo-950 absolute flex items-end justify-center text-center p-0 m-0  s">
-              <p className="p-2 font-sans text-white">{manhwaRec.title}</p>
-            </div>
-          </div>
-        ))}
+      <div className="top-manhwa-container w-full h-3/10 text-center">
+        <p className="p-2 font-sans text-white">
+          There is no manhwa to display
+        </p>
       </div>
     );
   }
+
+  const topTenManhwa: RankDetails[] = topManhwaResults.slice(0, 10);
+
+  return (
+    <div className="top-manhwa-container w-full h-3/10 carousel carousel-center p space-x-4 bg-neutral rounded-box bg-transparent overflow-y-hidden flex align-center py-4 mb-4">
+      {topTenManhwa.map((manhwaRec) => (
+        <div
+          key={manhwaRec.slug}
+          className="carousel-item w-2/6 h-full p-0 m-0 flex flex-col justify-center relative indicator first:pl-0 hover:cursor-pointer"
+        >
+          <span className="indicator-item badge badge-secondary px-2.5 py-3.5 border-none bg-purple-1000 text-white font-semibold font-sans">
+            {topManhwaResults.indexOf(manhwaRec) + 1}
+          </span>
+          <img
+            src={`https://meo3.comick.pictures/${manhwaRec.md_covers[0].b2key}`}
+            alt="Manga Cover Image"
+            className="rounded-box h-full w-full shadow-custom"
+          ></img>
+          <div className="image-ovelay h-full w-full rounded-box bg-gradient-to-t from-indigo-950 absolute flex items-end justify-center text-center p-0 m-0  s">
+            <p className="p-2 font-sans text-white">{manhwaRec.title}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 function TopManhua() {
@@ -103,30 +117,38 @@ function TopManhua() {
     console.error("An unexpected error occurred: " + error);
   }
 
-  if (topManhuaResults) {
-    const topTenManhua: RankDetails[] = topManhuaResults.slice(0, 10);
-
+  if (!topManhuaResults) {
     return (
-      <div className="top-manhua-container w-full h-3/10 carousel carousel-center p space-x-4 bg-neutral rounded-box bg-transparent overflow-y-hidden flex align-center py-4">
-        {topTenManhua.map((manhuaRec) => (
-          <div
-            key={manhuaRec.slug}
-            className="carousel-item w-2/6 h-full p-0 m-0 flex flex-col justify-center relative indicator first:pl-0 hover:cursor-pointer"
-          >
-            <span className="indicator-item badge badge-secondary px-2.5 py-3.5 border-none bg-purple-1000 text-white font-bold font-sans text-md">
-              {topManhuaResults.indexOf(manhuaRec) + 1}
-            </span>
-            <img
-              src={`https://meo3.comick.pictures/${manhuaRec.md_covers[0].b2key}`}
-              alt="Manga Cover Image"
-              className="rounded-box h-full w-full shadow-custom"
-            ></img>
-            <div className="image-ovelay h-full w-full rounded-box bg-gradient-to-t from-indigo-950 absolute flex items-end justify-center text-center p-0 m-0">
-              <p className="p-2 font-sans text-white">{manhuaRec.title}</p>
-            </div>
-          </div>
-        ))}
+      <div className="top-manhua-container w-full h-3/10 text-center">
+        <p className="p-2 font-sans text-white">
+          There is no manhua to display
+        </p>
       </div>
     );
   }
+
+  const topTenManhua: RankDetails[] = topManhuaResults.slice(0, 10);
+
+  return (
+    <div className="top-manhua-container w-full h-3/10 carousel carousel-center p space-x-4 bg-neutral rounded-box bg-transparent overflow-y-hidden flex align-center py-4">
+      {topTenManhua.map((manhuaRec) => (
+        <div
+          key={manhuaRec.slug}
+          className="carousel-item w-2/6 h-full p-0 m-0 flex flex-col justify-center relative indicator first:pl-0 hover:cursor-pointer"
+        >
+          <span className="indicator-item badge badge-secondary px-2.5 py-3.5 border-none bg-purple-1000 text-white font-bold font-sans text-md">
+            {topManhuaResults.indexOf(manhuaRec) + 1}
+          </span>
+          <img
+            src={`https://meo3.comick.pictures/${manhuaRec.md_covers[0].b2key}`}
+            alt="Manga Cover Image"
+            className="rounded-box h-full w-full shadow-custom"
+          ></img>
+          <div className="image-ovelay h-full w-full rounded-box bg-gradient-to-t from-indigo-950 absolute flex items-end justify-center text-center p-0 m-0">
+            <p className="p-2 font-sans text-white">{manhuaRec.title}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
