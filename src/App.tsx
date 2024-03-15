049d2a7ff9263f6df32a74122da9e23409fc80bb
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-03-15 20:54:12 +0200

    feat: :sparkles: Added Home Component
    Added component to represent the landing page and included basic components needed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import "./style/App.css";
 import Home from "./components/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <div className="bg-blue-purple h-screen w-full px-6">
+      <div className="header-container h-1/10"></div>
+      <div className="content-container h-4/5 overflow-y-scroll scroll-m-0 scroll-p-0 scrol">
+        <Home />
+      </div>
+      <div className="footer-container  h-1/10"></div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,120 @@
-import TopManga from "./TopManga";
+import { useQuery } from "@tanstack/react-query";
+import { RankDetails } from "../models/state";
+import { fetchTopManga, fetchTopManhwa, fetchTopManhua } from "../services/api";
 
 function Home() {
   return (
     <>
-      <h1>Hello Manga</h1>
+      <h2 className="text-white font-semibold text-xl">Top Manga</h2>
       <TopManga />
+      <h2 className="text-white font-semibold text-xl">Top Manhwa</h2>
+      <TopManhwa />
+      <h2 className="text-white font-semibold text-xl">Top Manhua</h2>
+      <TopManhua />
     </>
   );
 }
 
 export default Home;
+
+function TopManga() {
+  const { data: topMangaResults } = useQuery({
+    queryKey: [`fetchTopManga`],
+    queryFn: () => fetchTopManga(),
+  });
+
+  if (topMangaResults) {
+    const topTenManga: RankDetails[] = topMangaResults.slice(0, 10);
+
+    return (
+      <div className="top-manga-container w-full h-3/10 carousel carousel-center p space-x-4 bg-neutral rounded-box bg-transparent overflow-y-hidden flex align-center py-4 mb-4">
+        {topTenManga.map((mangaRec) => (
+          <div
+            key={mangaRec.slug}
+            className="carousel-item w-2/6 h-full p-0 m-0 flex flex-col justify-center relative indicator first:pl-0 hover:cursor-pointer"
+          >
+            <span className="indicator-item badge badge-secondary px-2.5 py-3.5 border-none bg-purple-1000 text-white font-semibold font-sans">
+              {topMangaResults.indexOf(mangaRec) + 1}
+            </span>
+            <img
+              src={`https://meo3.comick.pictures/${mangaRec.md_covers[0].b2key}`}
+              alt="Manga Cover Image"
+              className="rounded-box h-full w-full shadow-custom"
+            ></img>
+            <div className="image-ovelay h-full w-full rounded-box bg-gradient-to-t from-indigo-950 absolute flex items-end justify-center text-center p-0 m-0  s">
+              <p className="p-2 font-sans text-white">{mangaRec.title}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+function TopManhwa() {
+  const { data: topManhwaResults } = useQuery({
+    queryKey: [`fetchTopManhwa`],
+    queryFn: () => fetchTopManhwa(),
+  });
+
+  if (topManhwaResults) {
+    const topTenManga: RankDetails[] = topManhwaResults.slice(0, 10);
+
+    return (
+      <div className="top-manhwa-container w-full h-3/10 carousel carousel-center p space-x-4 bg-neutral rounded-box bg-transparent overflow-y-hidden flex align-center py-4 mb-4">
+        {topTenManga.map((mangaRec) => (
+          <div
+            key={mangaRec.slug}
+            className="carousel-item w-2/6 h-full p-0 m-0 flex flex-col justify-center relative indicator first:pl-0 hover:cursor-pointer"
+          >
+            <span className="indicator-item badge badge-secondary px-2.5 py-3.5 border-none bg-purple-1000 text-white font-semibold font-sans">
+              {topManhwaResults.indexOf(mangaRec) + 1}
+            </span>
+            <img
+              src={`https://meo3.comick.pictures/${mangaRec.md_covers[0].b2key}`}
+              alt="Manga Cover Image"
+              className="rounded-box h-full w-full shadow-custom"
+            ></img>
+            <div className="image-ovelay h-full w-full rounded-box bg-gradient-to-t from-indigo-950 absolute flex items-end justify-center text-center p-0 m-0  s">
+              <p className="p-2 font-sans text-white">{mangaRec.title}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+function TopManhua() {
+  const { data: topManhuaResults } = useQuery({
+    queryKey: [`fetchTopManhua`],
+    queryFn: () => fetchTopManhua(),
+  });
+
+  if (topManhuaResults) {
+    const topTenManhua: RankDetails[] = topManhuaResults.slice(0, 10);
+
+    return (
+      <div className="top-manhua-container w-full h-3/10 carousel carousel-center p space-x-4 bg-neutral rounded-box bg-transparent overflow-y-hidden flex align-center py-4">
+        {topTenManhua.map((mangaRec) => (
+          <div
+            key={mangaRec.slug}
+            className="carousel-item w-2/6 h-full p-0 m-0 flex flex-col justify-center relative indicator first:pl-0 hover:cursor-pointer"
+          >
+            <span className="indicator-item badge badge-secondary px-2.5 py-3.5 border-none bg-purple-1000 text-white font-bold font-sans text-md">
+              {topManhuaResults.indexOf(mangaRec) + 1}
+            </span>
+            <img
+              src={`https://meo3.comick.pictures/${mangaRec.md_covers[0].b2key}`}
+              alt="Manga Cover Image"
+              className="rounded-box h-full w-full shadow-custom"
+            ></img>
+            <div className="image-ovelay h-full w-full rounded-box bg-gradient-to-t from-indigo-950 absolute flex items-end justify-center text-center p-0 m-0">
+              <p className="p-2 font-sans text-white">{mangaRec.title}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
