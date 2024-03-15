bce7f3dade8b9e9682ef5cbb76b877d574c3544a
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-03-15 20:52:56 +0200

    feat: :wrench: Added custom styles needed for UI
    Added custom colours, box shadow and height needed for layout and UI.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        custom: "6px 3px 10px -5px rgba(255,255,255,0.51)",
+      },
+      colors: {
+        "blue-purple": "rgb(0, 8, 52)",
+        "purple-1000": "rgb(97, 83, 204)",
+      },
+      height: {
+        "1/10": "10%",
+        "3/10": "30%",
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
