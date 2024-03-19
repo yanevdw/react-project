3bf6fa69ec7bd67c798ad8584bde88e262f2ec21
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-03-19 20:08:48 +0200

    feat: :construction: Added temporary header
    Added a header as placeholder for the name of the project

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import Home from "./components/Home";
 function App() {
   return (
     <div className="bg-blue-purple h-screen w-full px-6">
-      <div className="header-container h-1/10"></div>
+      <div className="header-container h-1/10 flex items-center">
+        <h1 className="text-white font-sans text-2xl font-semibold">
+          Cool Name Pending
+        </h1>
+      </div>
       <div className="content-container h-4/5 overflow-y-scroll scroll-m-0 scroll-p-0 scrol">
         <Home />
       </div>
