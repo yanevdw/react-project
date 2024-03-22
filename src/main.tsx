87aa845e08dba40ad410179cf34797c09afb5adf
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-03-22 15:51:37 +0200

    feat: :fire: Remove unused import
    Removed the Link import from TanStack Router as it is not used

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style/index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { RouterProvider, createRouter, Link } from "@tanstack/react-router";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen.ts";
 
 const queryClient = new QueryClient();
