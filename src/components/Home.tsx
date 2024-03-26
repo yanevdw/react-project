bc9fec296ab6117618420474bb6db71302326d2a
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-03-26 14:31:39 +0200

    feat: :bug: Added safety checks
    Added safety checks for results as well as elemnts of the results array

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -58,18 +58,18 @@ function TopComicComponent({ comicType }: { comicType: string }) {
       >
         {topTenComics.map((comicRec: RankDetails) => (
           <div
-            key={comicRec.slug}
+            key={comicRec?.slug}
             className="carousel-item w-2/6 h-full p-0 m-0 flex flex-col justify-center relative first:pl-0 hover:cursor-pointer md:w-1/10 lg:w-1/10"
             onClick={() =>
               (
                 document.getElementById(
-                  `${comicType.toLowerCase()}-comic-popup-${comicRec.slug}`
+                  `${comicType.toLowerCase()}-comic-popup-${comicRec?.slug}`
                 ) as HTMLDialogElement
               ).showModal()
             }
           >
             <dialog
-              id={`${comicType.toLowerCase()}-comic-popup-${comicRec.slug}`}
+              id={`${comicType.toLowerCase()}-comic-popup-${comicRec?.slug}`}
               className="comic-popup border-none outline-none w-90/100 h-3/5 rounded-lg bg-frost backdrop-blur  md:w-3/5 lg:w-3/5"
             >
               <form method="dialog">
@@ -77,7 +77,7 @@ function TopComicComponent({ comicType }: { comicType: string }) {
                   ✕
                 </button>
               </form>
-              <ComicPopup comicSlug={comicRec.slug} />
+              <ComicPopup comicSlug={comicRec?.slug} />
             </dialog>
             {/* This code may be used in a later stage, if not, I will remove it. */}
 
@@ -85,12 +85,12 @@ function TopComicComponent({ comicType }: { comicType: string }) {
             {topMangaResults.indexOf(mangaRec) + 1}
           </span> */}
             <img
-              src={`https://meo3.comick.pictures/${comicRec.md_covers[0].b2key}`}
+              src={`https://meo3.comick.pictures/${comicRec?.md_covers?.[0]?.b2key}`}
               alt={`${comicType} Cover Image`}
               className="rounded-lg h-full w-full shadow-custom"
             ></img>
             <div className="image-overlay h-full w-full rounded-lg bg-gradient-to-t from-plum absolute flex items-end justify-center text-center p-0 m-0">
-              <p className="p-2 font-sans text-white">{comicRec.title}</p>
+              <p className="p-2 font-sans text-white">{comicRec?.title}</p>
             </div>
           </div>
         ))}
