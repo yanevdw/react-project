c7bfc15266715563e1d207c9d246ba6266833bc5
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-03-31 00:09:57 +0200

    feat: :lipstick: Updated and fixed styling for navbar
    Adjusted height to fill to bottom and edited items spacing and sizing

--- src/components/MobileNavigation.tsx
+++ src/components/MobileNavigation.tsx
@@ -4,19 +4,19 @@ import { FaSearch } from "react-icons/fa";
 
 function MobileNavigation() {
   return (
-    <div className="w-full h-16 mt-4 border-none outline-none bg-gray-600/35 flex flex-row items-center justify-center text-center align-self-end">
+    <div className="w-full h-full mt-4 border-none outline-none bg-gray-600/35 flex flex-row items-center justify-center text-center align-self-end">
       <Link
         to="/"
-        className="w-1/2 h-full flex flex-col items-center justify-center gap-1 text-white [&.active]:border-t-2 hover:cursor-pointer"
+        className="w-1/2 h-full flex flex-col items-center justify-center gap-2 text-white [&.active]:border-t-2 hover:cursor-pointer"
       >
-        <FaHome />
+        <FaHome size={18} />
         Home
       </Link>
       <Link
         to="/explore"
-        className="w-1/2 h-full flex flex-col items-center justify-center gap-1 text-white [&.active]:border-t-2 hover:cursor-pointer"
+        className="w-1/2 h-full flex flex-col items-center justify-center gap-2 text-white [&.active]:border-t-2 hover:cursor-pointer"
       >
-        <FaSearch />
+        <FaSearch size={18} />
         Explore
       </Link>
     </div>
