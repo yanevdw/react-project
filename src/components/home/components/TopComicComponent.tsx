0866c9d502dd82967d2a356ba43bd7f83e0fce9b
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-03-31 00:03:58 +0200

    feat: :art: Improved loading UI and added isPending prop to response
    Upgraded loader and also added isPending to response props and display loader for this as well

--- src/components/home/components/TopComicComponent.tsx
+++ src/components/home/components/TopComicComponent.tsx
@@ -3,13 +3,40 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchTopComics } from "../../../services/api";
 import ComicPopup from "../../ComicPopup";
 function TopComicComponent({ comicType }: { comicType: string }) {
-  const { data: topComicResults, error } = useQuery({
+  const {
+    data: topComicResults,
+    error,
+    isLoading,
+    isPending,
+  } = useQuery({
     queryKey: [`fetchTop${comicType}`],
     queryFn: () => fetchTopComics(comicType.toLowerCase()),
   });
 
   if (error) {
     console.error("An unexpected error occurred: " + error);
+  }
+
+  if (isLoading || isPending) {
+    return (
+      <div
+        className={`top-${comicType.toLowerCase()}-container w-full h-3/10 flex flex-col items-center justify-center`}
+      >
+        <div className="loader-container w-full flex flex-row justify-center py-4">
+          <span className="loading loading-spinner text-primary"></span>
+          <span className="loading loading-spinner text-secondary"></span>
+          <span className="loading loading-spinner text-accent"></span>
+          <span className="loading loading-spinner text-neutral"></span>
+          <span className="loading loading-spinner text-info"></span>
+          <span className="loading loading-spinner text-success"></span>
+          <span className="loading loading-spinner text-warning"></span>
+          <span className="loading loading-spinner text-error"></span>
+        </div>
+        <h2 className="font-semibold text-white text-xl text-center">
+          Building the web
+        </h2>
+      </div>
+    );
   }
 
   if (
