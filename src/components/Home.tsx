9b7e1921abc552d1b5b5167992ad1220c8e6ddfb
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-03-30 22:34:56 +0200

    feat: :lipstick: Updated UI for the home page
    Edited headers, adjusted sizing of popup for different screen sizes and edited styling for carousel items.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,11 +6,11 @@ import ComicPopup from "./ComicPopup";
 function Home() {
   return (
     <>
-      <h2 className="text-white font-semibold text-xl">Top Manga</h2>
+      <h2 className="text-white font-semibold text-xl">Top 10 Manga</h2>
       <TopComicComponent comicType="Manga" />
-      <h2 className="text-white font-semibold text-xl">Top Manhwa</h2>
+      <h2 className="text-white font-semibold text-xl">Top 10 Manhwa</h2>
       <TopComicComponent comicType="Manhwa" />
-      <h2 className="text-white font-semibold text-xl">Top Manhua</h2>
+      <h2 className="text-white font-semibold text-xl">Top 10 Manhua</h2>
       <TopComicComponent comicType="Manhua" />
     </>
   );
@@ -51,6 +51,8 @@ function TopComicComponent({ comicType }: { comicType: string }) {
     .filter((comic: RankDetails) => comic.md_covers?.[0]?.b2key)
     .slice(0, 10);
 
+  console.log(topTenComics);
+
   return (
     <>
       <div
@@ -59,7 +61,7 @@ function TopComicComponent({ comicType }: { comicType: string }) {
         {topTenComics.map((comicRec: RankDetails) => (
           <div
             key={comicRec?.slug}
-            className="carousel-item w-2/6 h-full p-0 m-0 flex flex-col justify-center relative first:pl-0 hover:cursor-pointer md:w-1/10 lg:w-1/10"
+            className="carousel-item w-30/100 h-full p-0 m-0 flex flex-col justify-center relative first:pl-0 hover:cursor-pointer md:w-1/6 lg:w-1/10"
             onClick={() =>
               (
                 document.getElementById(
@@ -70,27 +72,24 @@ function TopComicComponent({ comicType }: { comicType: string }) {
           >
             <dialog
               id={`${comicType.toLowerCase()}-comic-popup-${comicRec?.slug}`}
-              className="comic-popup border-none outline-none w-90/100 h-3/5 rounded-lg bg-frost backdrop-blur  md:w-3/5 lg:w-3/5"
+              className="comic-popup border-none outline-none  h-3/5 rounded-lg bg-frost backdrop-blur xs:w-full sm:w-3/5 py-3 md:w-1/2 lg:w-2/5 xl:w-3/10"
             >
               <form method="dialog">
-                <button className="btn btn-sm btn-circle btn-ghost absolute right-7 top-7  p-0 m-0 flex items-center justify-center outline-none">
+                <button className="btn btn-sm btn-circle btn-ghost absolute right-7 top-7 p-0 m-0 flex items-center justify-center outline-none">
                   ✕
                 </button>
               </form>
               <ComicPopup comicSlug={comicRec?.slug} />
             </dialog>
-            {/* This code may be used in a later stage, if not, I will remove it. */}
-
-            {/* <span className="indicator-item badge badge-secondary px-2.5 py-3.5 border-none bg-purple-1000 text-white font-semibold font-sans">
-            {topMangaResults.indexOf(mangaRec) + 1}
-          </span> */}
             <img
               src={`https://meo3.comick.pictures/${comicRec?.md_covers?.[0]?.b2key}`}
               alt={`${comicType} Cover Image`}
-              className="rounded-lg h-full w-full shadow-custom"
+              className="rounded-lg h-full w-full shadow-md shadow-gray-600"
             ></img>
             <div className="image-overlay h-full w-full rounded-lg bg-gradient-to-t from-plum absolute flex items-end justify-center text-center p-0 m-0">
-              <p className="p-2 font-sans text-white">{comicRec?.title}</p>
+              <p className="p-2 text-white font-sans font-semibold">
+                {comicRec?.title}
+              </p>
             </div>
           </div>
         ))}
