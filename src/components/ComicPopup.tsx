f7f35f9ef8abd08ad73c5e2fa43731af7d7f9784
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-04-29 16:17:31 +0200

    feat: :recycle: Updated styling and sides of comic card
    Switched the card sides and edited styling bugs.

--- src/components/ComicPopup.tsx
+++ src/components/ComicPopup.tsx
@@ -65,26 +65,7 @@ function ComicPopup({ comicSlug }: { comicSlug: string }) {
           animate={{ rotateY: cardFlipped ? 180 : 360 }}
           transition={{ duration: 0.6, animationDirection: "normal" }}
         >
-          <div className="comic-popup-front absolute flex flex-row justify-end w-full h-full bg-frost backdrop-blur rounded-lg p-3">
-            <img
-              src={`https://meo3.comick.pictures/${comicContentResults.comic.md_covers[0].b2key}`}
-              alt="Manga Cover Image"
-              className="h-full w-full object-cover object-top rounded-md"
-            />
-            <div className="controller-container absolute flex flex-row w-1/5 h-fit gap-4 justify-center m-4">
-              <GiClick
-                className="bg-purple-1000 w-2/5  flex items-end p-1.5 text-4xl rounded-2xl text-white md:hidden"
-                // size={40}
-              />
-              <form method="dialog">
-                <button className="btn bg-plum-300 btn-sm btn-circle btn-ghost text-lg leading-8 h-fit  m-0 flex items-center justify-center outline-none text-center md:absolute">
-                  ✕
-                </button>
-              </form>
-            </div>
-          </div>
-
-          <div className="comic-popup-back absolute w-full h-full bg-frost backdrop-blur rounded-lg p-3">
+          <div className="comic-popup-front absolute w-full h-full bg-frost backdrop-blur rounded-lg p-3">
             <div className="comic-info-container w-full h-full flex flex-col bg-plum-500 p-4 rounded-lg">
               <div className="comic-quick-facts-container mb-4 w-full h-1/10 flex justify-between items-center gap-4">
                 <div className="comic-popup-header flex flex-row justify-start items-center gap-4">
@@ -94,12 +75,13 @@ function ComicPopup({ comicSlug }: { comicSlug: string }) {
                   {/* Sometimes the follow count is not null but 0, so I am accommodating this by hiding the follow count in the event that this happens */}
                   {comicContentResults.comic.follow_count !== 0 ||
                   comicContentResults.comic.title.length < 10 ? (
-                    <label className="h-fit bg-plum-300 bg-blur px-4 py-1 rounded-3xl flex gap-2 text-center">
-                      <IoPerson className="mt-1" />
-                      {comicContentResults.comic.follow_count}
-                    </label>
+                      <label className="h-fit bg-plum-300 bg-blur px-4 py-1 rounded-3xl flex gap-2 text-center">
+                        <IoPerson className="mt-1" />
+                        {comicContentResults.comic.follow_count}
+                      </label>
                   ) : null}
                 </div>
+
 
                 <form method="dialog">
                   <button className="btn btn-sm btn-circle btn-ghost p-0 m-0 flex items-center justify-center outline-none">
@@ -109,22 +91,22 @@ function ComicPopup({ comicSlug }: { comicSlug: string }) {
               </div>
               <div className="comic-image-container w-full h-2/5">
                 <img
-                  src={`https://meo3.comick.pictures/${comicContentResults.comic.md_covers[0].b2key}`}
-                  alt="Manga Cover Image"
-                  className="h-full w-full object-cover object-top rounded-md"
+                    src={`https://meo3.comick.pictures/${comicContentResults.comic.md_covers[0].b2key}`}
+                    alt="Manga Cover Image"
+                    className="h-full w-full object-cover object-top rounded-md"
                 />
               </div>
               <div className="comic-genres-container flex flex-row h-fit justify-start items-start mt-4 gap-2 flex-wrap md:justify-start md:gap-4 lg:justify-start lg:gap-4 overflow-x-clip">
                 {comicGenres.map((genre) => (
-                  <label
-                    className="bg-purple-power px-3 py-1 rounded-3xl"
-                    key={genre.md_genres.slug}
-                  >
-                    {genre.md_genres.name}
-                  </label>
+                    <label
+                        className="bg-purple-power px-3 py-1 rounded-3xl"
+                        key={genre.md_genres.slug}
+                    >
+                      {genre.md_genres.name}
+                    </label>
                 ))}
               </div>
-              <div className="comic-description-container w-full mb-8 md:mb-2 h-1/5 md:h-3/10">
+              <div className="comic-description-container w-full mb-8 md:mb-2 h-[15%] md:h-3/10">
                 <h3 className="text-white font-sans font-semibold font-md mt-2">
                   Description
                 </h3>
@@ -132,15 +114,39 @@ function ComicPopup({ comicSlug }: { comicSlug: string }) {
                   {comicContentResults.comic.desc}
                 </p>
               </div>
-              <div className="comic-popup-button-container h-1/10 flex justify-end">
+              <div className="comic-popup-button-container h-1/10 flex justify-between">
+                <GiClick
+                    className="bg-purple-1000 w-fit  flex items-end p-2 text-4xl rounded-2xl text-white md:hidden"
+                    // size={40}
+                />
                 <Link
-                  to={`/comic/${comicContentResults.comic.slug}`}
-                  className="bg-blue-munsell rounded-lg px-2 text-white flex items-center font-semibold"
+                    to={`/comic/${comicContentResults.comic.slug}`}
+                    className="bg-blue-munsell rounded-lg px-2 text-white flex items-center font-semibold"
                 >
                   Read Comic
                 </Link>
-              </div>
+              </div></div>
+          </div>
+
+          <div className="comic-popup-back absolute flex flex-row justify-end w-full h-full bg-frost backdrop-blur rounded-lg p-3">
+            <img
+                src={`https://meo3.comick.pictures/${comicContentResults.comic.md_covers[0].b2key}`}
+                alt="Manga Cover Image"
+                className="h-full w-full object-cover object-top rounded-md"
+            />
+            <div className="controller-container absolute flex flex-row w-2/5 h-fit gap-2 justify-end m-4">
+              <GiClick
+                  className="bg-purple-1000 w-fit  flex items-end p-2 text-4xl rounded-2xl text-white md:hidden"
+                  // size={40}
+              />
+              <form method="dialog">
+                <button
+                    className="btn bg-plum-300 btn-sm btn-circle btn-ghost text-lg leading-8 h-fit  m-0 flex items-center justify-center outline-none text-center md:absolute right-1">
+                  ✕
+                </button>
+              </form>
             </div>
+
           </div>
         </motion.div>
       </div>
