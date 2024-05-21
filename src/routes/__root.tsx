4ed8fd73a15acdec277761840050d0a28ac3be0d
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-05-21 07:02:43 +0200

    feat: :recycle: Updated routing layout
    Added additional check to check on hide the header when the page is on the comic page to adapt to new styling.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -7,36 +7,39 @@ export const Route = createRootRoute({
   component: () => (
     <>
       <div className="h-screen w-full bg-plum overflow-y-hidden">
-        <div className="header-container px-6 h-1/10 flex items-center justify-between">
-          <Link to="/" className="text-white font-sans text-2xl font-semibold hover:cursor-pointer">
-            Jorōgumo
-          </Link>
-          {window.location.pathname.includes("chapter") ? (
-            <div className="web-nav-container text-white w-3/10 block">
-              <DesktopNavigation />
-            </div>
-          ) : (
-            <div className="web-nav-container text-white w-3/10 hidden md:block">
-              <DesktopNavigation />
-            </div>
-          )}
-        </div>
-        {window.location.pathname.includes("chapter") ? (
-          <div className="content-container h-90/100 overflow-y-scroll scroll-m-0 scroll-p-0">
-            <Outlet />
-          </div>
-        ) : (
-          <div className="content-container px-6 h-4/5 overflow-y-scroll scroll-m-0 scroll-p-0 md:h-90/100">
-            <Outlet />
-          </div>
-        )}
+          {window.location.pathname.includes("comic") && !window.location.pathname.includes("chapter") ? null : (
+              <div className="header-container px-6 h-1/10 flex items-center justify-between">
 
-        {window.location.pathname.includes("chapter") ? (
-          <div className="hidden">
-            <div className="mobile-nav-container w-full h-full hidden">
-              <MobileNavigation />
-            </div>
-          </div>
+                  <Link to="/" className="text-white font-sans text-2xl font-semibold hover:cursor-pointer">
+                      Jorōgumo
+                  </Link>
+                  {window.location.pathname.includes("chapter") ? (
+                      <div className="web-nav-container text-white w-3/10 block">
+                          <DesktopNavigation/>
+                      </div>
+                  ) : (
+                      <div className="web-nav-container text-white w-3/10 hidden md:block">
+                          <DesktopNavigation/>
+                      </div>
+                  )}
+              </div>)}
+
+          {window.location.pathname.includes("chapter") || window.location.pathname.includes("comic") ? (
+              <div className="content-container h-90/100 overflow-y-scroll scroll-m-0 scroll-p-0">
+                  <Outlet/>
+              </div>
+          ) : (
+              <div className="content-container h-4/5 px-6 overflow-y-scroll scroll-m-0 scroll-p-0 md:h-90/100">
+                  <Outlet/>
+              </div>
+          )}
+
+          {window.location.pathname.includes("chapter") ? (
+              <div className="hidden">
+                  <div className="mobile-nav-container w-full h-full hidden">
+                      <MobileNavigation/>
+                  </div>
+              </div>
         ) : (
           <div className="footer-container w-full h-1/10 block md:hidden">
             <div className="mobile-nav-container w-full h-full">
