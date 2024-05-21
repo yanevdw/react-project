0a491ba7fa687615d032802ef7b47e8ddf2463f6
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-05-21 07:12:18 +0200

    feat: :sparkles: Added pagination
    Added pagination to limit amount of chapters displayed in the list and also reduce the number of request needed to be made at a time.

--- src/components/comic/components/ChapterList.tsx
+++ src/components/comic/components/ChapterList.tsx
@@ -1,8 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
-import { fetchComicChapters } from "../../../services/api";
+import { fetchComicChapters} from "../../../services/api";
 import { Chapter } from "../../../models/common-types";
 import { Link } from "@tanstack/react-router";
 import ContentLoader from "../../ContentLoader";
+import ChapterListItem from "./ChapterListItem.tsx";
+import {useState} from "react";
+import Pagination from "./Pagination.tsx";
 
 function ChapterList({
   comicHid,
@@ -13,6 +16,10 @@ function ChapterList({
   comicSlug: string;
   chapterCount: number;
 }) {
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [chaptersPerPage] = useState(10);
+
   const {
     data: comicChapters,
     error,
@@ -43,17 +50,45 @@ function ChapterList({
     );
   }
 
+
+  const filteredChapters = comicChapters?.chapters?.filter((chapter: Chapter) => chapter.title === null);
+
+  for (const chapter of filteredChapters) {
+
+
+    for (const chap of filteredChapters) {
+
+      if (chapter.chap === chap.chap) {
+
+        if (chapter.up_count > chap.up_count) {
+          // console.log(chapter, chapter.up_count);
+          // console.log(chap, chap.up_count);
+          filteredChapters?.splice(filteredChapters?.indexOf(chap),1);
+        } else {
+          filteredChapters?.splice(filteredChapters?.indexOf(chapter),1);
+        }
+      }
+    }
+  }
+
+  const lastChapterIndex = currentPage * chaptersPerPage;
+  const firstChapterIndex = lastChapterIndex - chaptersPerPage;
+
+  const currentChapters = filteredChapters.slice(firstChapterIndex, lastChapterIndex);
+
   return (
-    <div className="w-full h-2/5 flex flex-col gap-2">
-      {comicChapters.chapters.map((chapter: Chapter) => (
+    <div className="w-full h-fit pb-3 flex flex-col gap-3 overflow-x-hidden">
+      {currentChapters.map((chapter: Chapter) => (
         <Link
-          className="w-full h-1/5 pl-4 py-4 rounded-lg bg-purple-grey shadow-md hover:-ml-1 text-white"
+          className="w-full h-4/5 rounded-lg bg-purple-grey shadow-md hover:-ml-1 text-white"
           key={chapter.hid}
           to={`/comic/${comicSlug}/chapter/${chapter.hid}`}
         >
-          {chapter.title ? `${chapter.title}` : `Chapter ${chapter.chap}`}
+          <ChapterListItem chapterHid={chapter.hid} />
         </Link>
       ))}
+
+      <Pagination totalChapters={filteredChapters?.length} chaptersPerPage={chaptersPerPage} setCurrentPage={setCurrentPage} />
     </div>
   );
 }
